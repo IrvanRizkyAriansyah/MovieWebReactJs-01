c7c25a069e2fe09ee5348d3cd45d3c79ab3c8243
IrvanRizkyAriansyah <92045164+IrvanRizkyAriansyah@users.noreply.github.com>
2022-10-12 16:16:08 +0700

    Login

---
+++ src/component/Login.js
@@ -0,0 +1,85 @@
+import { Button, Modal, Checkbox, Form, Input } from 'antd';
+import React, { useState } from 'react';
+import ButtonBorder from './ButtonBorder';
+import { LockOutlined, UserOutlined } from '@ant-design/icons';
+
+const Login = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const onFinish = (values) => {
+    console.log('Received values of form: ', values);
+  };
+
+  const showModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleOk = () => {
+    setIsModalOpen(false);
+  };
+
+  const handleCancel = () => {
+    setIsModalOpen(false);
+  };
+
+  return (
+    <>
+      <ButtonBorder title='Login' link={showModal} />
+      <Modal title="Login" open={isModalOpen} onOk={handleOk} onCancel={handleCancel}>
+        <Form
+      name="normal_login"
+      className="login-form"
+      initialValues={{
+        remember: true,
+      }}
+      onFinish={onFinish}
+    >
+      <Form.Item
+        name="username"
+        rules={[
+          {
+            required: true,
+            message: 'Please input your Username!',
+          },
+        ]}
+      >
+        <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="Username" />
+      </Form.Item>
+      <Form.Item
+        name="password"
+        rules={[
+          {
+            required: true,
+            message: 'Please input your Password!',
+          },
+        ]}
+      >
+        <Input
+          prefix={<LockOutlined className="site-form-item-icon" />}
+          type="password"
+          placeholder="Password"
+        />
+      </Form.Item>
+      <Form.Item>
+        <Form.Item name="remember" valuePropName="checked" noStyle>
+          <Checkbox>Remember me</Checkbox>
+        </Form.Item>
+
+        <a className="login-form-forgot" href="">
+          Forgot password
+        </a>
+      </Form.Item>
+
+      <Form.Item>
+        <Button type="primary" htmlType="submit" className="login-form-button">
+          Log in
+        </Button>
+        Or <a href="">register now!</a>
+      </Form.Item>
+    </Form>
+      </Modal>
+    </>
+  );
+};
+
+export default Login;
